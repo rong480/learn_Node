589c5d261f89c84d5586a460d750c4bdf2394c26
rongjiaqi <375107109@qq.com>
2018-11-15 21:10:56 +0800

    增加部分demo

---
+++ router.js
@@ -0,0 +1,5 @@
+function route (pathname) {
+  console.log('route name is ' + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -45,10 +45,13 @@
 var http = require("http");
 var url = require("url");
 
-function start () {
+function start (route) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log('pathname:' + pathname);
+
+		route(pathname);
+
 		response.writeHead(200, {'Content-Type':'text/plain'});
 		response.write('hello 333');
 		response.end();
