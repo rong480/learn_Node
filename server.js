b234d1d32285ceb2d7844a371f3d780bc919863b
rongjiaqi <375107109@qq.com>
2018-11-15 17:39:56 +0800

    增加部分demo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require("http");
+// ----------------------- demo1
+// var http = require("http");
 
 // http.createServer(function(request, response) {
 //    response.writeHead(200, {"Content-Type": "text/plain"});
@@ -9,13 +10,52 @@ var http = require("http");
 // var server = http.createServer();
 // server.listen(8888);
 
-function onRequest (request,response) {
-	console.log('111');
-	response.writeHead(200,{'Content-Type':'text-plain'});
-	response.write("hello");
-	response.end();
+// ----------------------- demo2
+// var http = require("http");
+//
+// function onRequest (request,response) {
+// 	console.log('recieved request');
+// 	response.writeHead(200,{'Content-Type':'text-plain'});
+// 	response.write("hello");
+// 	response.end();
+// }
+//
+// http.createServer(onRequest).listen(8888);
+//
+// console.log('server is starting!');
+
+// ----------------------- demo3 将server.js变成一个模块
+// var http = require("http");
+//
+// function start() {
+//   function onRequest (request,response) {
+// 		console.log('recieved request');
+// 		response.writeHead(200,{'Content-Type':'text-plain'});
+// 		response.write("hello 123");
+// 		response.end();
+// 	}
+//
+// 	http.createServer(onRequest).listen(8888);
+//   console.log('server is starting!');
+// }
+//
+// exports.start = start;
+
+// ----------------------- demo4 处理请求的url路径
+var http = require("http");
+var url = require("url");
+
+function start () {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log('pathname:' + pathname);
+		response.writeHead(200, {'Content-Type':'text/plain'});
+		response.write('hello 333');
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log('server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('222');
+exports.start = start;
